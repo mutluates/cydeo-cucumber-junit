2269197ba7e8ef954d99a331caa54166fc78b549
makbas06 <mutluates135@gmail.com>
2022-06-16 17:04:15 +0300

    update

--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinition.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinition.java
@@ -1,16 +1,41 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.DropdownPage;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTable_StepDefinition {
 
     @Then("user should see fruit I like")
     public void user_should_see_fruit_i_like(List<String> fruits) {
-
         System.out.println(fruits);
+    }
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        Driver.getDriver().get("https://practice.cydeo.com/dropdown");
+    }
+    DropdownPage dropdownPage = new DropdownPage();
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
 
+        Select select = new Select(dropdownPage.monthDropdown);
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+
+        //create a list of string and pass all of the actual web element options' string into that new list
+
+        List<String> actualOptions = new ArrayList<>();
+
+        for (WebElement each : actualOptionsAsWebElement) {
+            actualOptions.add(each.getText());
+        }
+        Assert.assertEquals(expectedMonths,actualOptions);
     }
 
 
--- src/test/resources/features/dataTables.feature
+++ src/test/resources/features/dataTables.feature
@@ -1,6 +1,6 @@
 Feature: Examples of Cucumber data tables implementations
-  Scenario: List of fruits I like
 
+  Scenario: List of fruits I like
     Then user should see fruit I like
       | kiwi        |
       | banana      |
@@ -9,3 +9,21 @@ Feature: Examples of Cucumber data tables implementations
       | mango       |
       | grape       |
       | pomegranate |
+
+  @wip
+  Scenario:  User should be able to see all 12 months in months
+  dropdown
+    Given User is on the dropdowns page of practice tool
+    Then User should see below info in month dropdown
+      | January   |
+      | February  |
+      | March     |
+      | April     |
+      | May       |
+      | June      |
+      | July      |
+      | August    |
+      | September |
+      | October   |
+      | November  |
+      | December  |
